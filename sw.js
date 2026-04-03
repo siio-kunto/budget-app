5cac07dea940732e4f73637b0e70bd40c28b6cce
Oswald H. König <hello@oswaldkoenig.ch>
2026-04-03 17:08:19 +0200

    S14: Abstraktionsschicht + PWA-Install + BUG-045/046/047/049

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'financebird-v2.14a-20260403';
+const CACHE_NAME = 'financebird-v2.14d-20260403';
 const ASSETS = [
   './financebird_v2.html',
   'https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,400;0,600;1,400&family=Plus+Jakarta+Sans:wght@400;500;600&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&family=Courier+Prime:wght@400;700&display=swap',
